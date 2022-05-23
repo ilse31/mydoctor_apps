3761488560398ef1b5c60566acfbcbc795addd55
ilse31 <58392119+ilse31@users.noreply.github.com>
2022-05-23 20:03:53 +0700

    add input button and form sign

---
+++ src/components/Link/index.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Link = () => {
+  return (
+    <View>
+      <Text>LinkComponent</Text>
+    </View>
+  );
+};
+
+export default Link;
+
+const styles = StyleSheet.create({});
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import GetStarted from './getStarted';
+import Splash from './splash';
+import SignIn from './SignIn';
+import Register from './Register';
+export {GetStarted, Splash, SignIn, Register};
